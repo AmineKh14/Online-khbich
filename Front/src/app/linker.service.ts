e526cde2e3aa75f2c1aae45246592d9a79d5487a
AmineKh14 <khamouda123@gmail.com>
2020-10-10 13:01:53 +0100

    adding linker service

---
+++ Front/src/app/linker.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinkerService {
+
+
+  Uri:string = 'http://localhost:3000/courses';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+
+  constructor(private http: HttpClient) { }
+
+  createCourse(data): Observable<any> {
+    let url = `${this.Uri}/create`;
+    return this.http.post(url, data)
+      .pipe(
+        catchError(this.errorMgmt)
+      )
+  }
+
+  getCourses() {
+    return this.http.get(`${this.Uri}`);
+  }
+
+  getCourse(id): Observable<any> {
+    let url = `${this.Uri}/read/${id}`;
+    return this.http.get(url, {headers: this.headers}).pipe(
+      map((res: Response) => {
+        return res || {}
+      }),
+      catchError(this.errorMgmt)
+    )
+  }
+
+  updateCourse(id, data): Observable<any> {
+    let url = `${this.Uri}/update/${id}`;
+    return this.http.put(url, data, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  deleteCourse(id): Observable<any> {
+    let url = `${this.Uri}/delete/${id}`;
+    return this.http.delete(url, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+}
